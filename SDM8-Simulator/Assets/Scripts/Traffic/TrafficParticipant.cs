357cb2f99d293206339fffe167215c195fdbb667
Jan Julius <jdelang@hotmail.com>
2019-10-22 14:21:37 +0200

    adjust: start working on stopping for stoplights for traffic participants

--- SDM8-Simulator/Assets/Scripts/Traffic/TrafficParticipant.cs
+++ SDM8-Simulator/Assets/Scripts/Traffic/TrafficParticipant.cs
@@ -23,8 +23,33 @@ namespace Assets.Scripts.Traffic
             if (Vector3.Distance(gameObject.transform.position, path.Points[currentNode + 1]) < 1)
                 currentNode++;
 
+            foreach(StopLight s in path.StopForStopLights)
+            {
+                if(Vector3.Distance(gameObject.transform.position, s.gameObject.transform.position) < 1
+                    && InFrontOfStopLight(s))
+                {
+                    if (s.Status == 0)
+                        return;
+                }
+            }
             Face(path.Points[currentNode + 1]);
             transform.position = Vector3.MoveTowards(gameObject.transform.position, path.Points[currentNode + 1], Speed * Time.deltaTime);
+        }
+
+        private bool InFrontOfStopLight(StopLight s)
+        {
+            var rot = s.transform.rotation.y;
+            if (rot != 0 || rot != 90 || rot != 180 || rot != 270)
+                Debug.LogError("Stoplight does not have the correct rotation");
+
+            if(s.transform.rotation.y == 0 && transform.position.z < s.transform.position.z
+                || s.transform.rotation.y == 90 && transform.position.x < s.transform.position.x
+                || s.transform.rotation.y == 180 && transform.position.z > s.transform.position.z
+                || s.transform.rotation.y == 270 && transform.position.x > s.transform.position.x)
+            {
+                return true;
+            }
+            return false;
         }
 
         public void SetPath(Path path)
@@ -35,5 +60,7 @@ namespace Assets.Scripts.Traffic
 
         public void Face(Vector3 faceTo)
             => transform.LookAt(faceTo);
+
+
     }
 }
