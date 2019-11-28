026ed85163f745b207f30fe1d57d4d5731405e08
Jan Julius <jdelang@hotmail.com>
2019-11-28 15:26:48 +0100

    add: bike

--- SDM8-Simulator/Assets/Scripts/Traffic/Participant/Biker.cs
+++ SDM8-Simulator/Assets/Scripts/Traffic/Participant/Biker.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Assets.Scripts.Traffic.Participant
 {
-    class Biker
+    class Biker : TrafficParticipant
     {
     }
 }
--- SDM8-Simulator/Assets/Scripts/Traffic/Participant/Walker.cs
+++ SDM8-Simulator/Assets/Scripts/Traffic/Participant/Walker.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Assets.Scripts.Traffic.Participant
 {
-    class Walker
+    class Walker : TrafficParticipant
     {
     }
 }
