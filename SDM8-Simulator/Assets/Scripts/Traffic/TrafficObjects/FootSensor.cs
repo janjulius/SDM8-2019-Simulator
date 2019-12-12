cd0f44354dc5bc36e9aff503197e34570b897477
dwarfexop <lars-schipper@hotmail.com>
2019-12-12 20:10:52 +0100

    Add: lane specific sensors

---
+++ SDM8-Simulator/Assets/Scripts/Traffic/TrafficObjects/FootSensor.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts.Traffic.Participant;
+
+namespace Assets.Scripts.Traffic.TrafficObjects
+{
+    internal class FootSensor : Sensor
+    {
+        private void OnTriggerEnter(UnityEngine.Collider collision)
+        {
+            Walker walker = collision.gameObject.GetComponent<Walker>();
+            if (walker != null)
+                SetStatus(1);
+        }
+
+        private void OnTriggerExit(UnityEngine.Collider collision)
+        {
+            Walker walker = collision.gameObject.GetComponent<Walker>();
+            if (walker != null)
+                SetStatus(0);
+        }
+    }
+}
