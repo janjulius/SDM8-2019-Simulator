d74147e8b278480c09b9b43b2a154decdf7beb5c
thosmas <thomas.tijsma@gmail.com>
2019-12-12 15:11:28 +0100

    fixed issue with sensor and some other small changes

--- SDM8-Simulator/Assets/Scripts/Traffic/TrafficObjects/RoadSensor.cs
+++ SDM8-Simulator/Assets/Scripts/Traffic/TrafficObjects/RoadSensor.cs
@@ -13,8 +13,6 @@ namespace Assets.Scripts.Traffic.TrafficObjects
             Car car = collision.gameObject.GetComponent<Car>();
             if (car != null)
                 SetStatus(1);
-            else
-                SetStatus(0);
         }
 
         private void OnTriggerExit(UnityEngine.Collider collision)
@@ -22,8 +20,6 @@ namespace Assets.Scripts.Traffic.TrafficObjects
             Car car = collision.gameObject.GetComponent<Car>();
             if (car != null)
                 SetStatus(0);
-            else
-                SetStatus(1);
         }
     }
 }
